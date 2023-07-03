d89cec139191e79404d256e6cf7cb7f677f3e2d4
Beatriz Bravo <beatriz.bravo-chavez@ten10.com>
2023-07-03 12:48:44 +0100

    Consuming bankHolidayUK using NewBuilder with method get

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,20 +9,23 @@ public class Main {
             // 1. Create an HttpClient object
             HttpClient client = HttpClient.newHttpClient();
 
-            // 2. Create an HttpRequest object with the desired API endpoint
+            // 2. Create an HttpRequest object with the desired API endpoint and request method
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("https://api.thecatapi.com/v1/images/search"))
-                    .header("x-api-key", "YOUR-API-KEY-HERE")
+                    .uri(URI.create("https://www.gov.uk/bank-holidays.json"))
+                    //.method("GET", HttpRequest.BodyPublishers.noBody()) //method is used as a shorthand to set the request method to GET.
+                    .GET() //method is used as a shorthand to set the request method to GET.
                     .build();
+
 
             // 3. Send the request synchronously and get the response
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
             // 4. Print the response
             System.out.println(response.body());
-            System.out.println(response.body());
 
         } catch (Exception e) {
+
+            System.out.println("URI is not valid");
             e.printStackTrace();
         }
     }
