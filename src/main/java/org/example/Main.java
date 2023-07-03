549c3a3f5cbece66fb7608695874fab1fca97d29
Beatriz Bravo <beatriz.bravo-chavez@ten10.com>
2023-07-03 12:15:41 +0100

    Consuming cat api using NewBuilder

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,31 +1,26 @@
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            // 1. Create a URL object with the desired API endpoint
-            URL url = new URL("https://www.gov.uk/bank-holidays.json");
+            // 1. Create an HttpClient object
+            HttpClient client = HttpClient.newHttpClient();
 
-            // 2. Open a connection to the URL
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            // 2. Create an HttpRequest object with the desired API endpoint
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://api.thecatapi.com/v1/images/search"))
+                    .header("x-api-key", "YOUR-API-KEY-HERE")
+                    .build();
 
-            // 3. Set the request method (GET, POST, etc.)
-            con.setRequestMethod("GET");
+            // 3. Send the request synchronously and get the response
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-            // 4. Read the response from the API
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuffer content = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
-            }
-            in.close();
-
-            // 5. Print the response
-            System.out.println(content.toString());
+            // 4. Print the response
+            System.out.println(response.body());
+            System.out.println(response.body());
 
         } catch (Exception e) {
             e.printStackTrace();
