f7026e01b0e3f423285d9158afce64375ed56341
Beatriz Bravo <beatriz.bravo-chavez@ten10.com>
2023-07-04 13:51:17 +0100

    Addel Jackson Library, created the class EventData, CountryDAta and BAnkholidayDAta

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,44 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 public class Main {
+
+    public static class EventData{
+        public String title;
+        public String date;
+        public String notes;
+        public String bunting;
+    }
+
+    public static class CountryData{
+      public String division;
+      public List<EventData>events;
+  }
+
+    public static class BankHolidayData{
+
+       @JsonProperty("england-and-wales")
+       public CountryData englandAnWales;
+
+       @JsonProperty("norther-ireland")
+       public CountryData northernIreland;
+       public CountryData scontland;
+   }
+
     public static void main(String[] args) {
         try {
-            // 1. Create an HttpClient object
+
             HttpClient client = HttpClient.newHttpClient();
 
-            // 2. Create an HttpRequest object with the desired API endpoint and request method
+
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create("https://www.gov.uk/bank-holidays.json"))
                     //.method("GET", HttpRequest.BodyPublishers.noBody()) //method is used as a shorthand to set the request method to GET.
@@ -18,10 +47,16 @@ public class Main {
 
 
             // 3. Send the request synchronously and get the response
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            var response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-            // 4. Print the response
-            System.out.println(response.body());
+            var body = response.body();
+            var objectMapper = new ObjectMapper();
+            var events = objectMapper.readTree(body).get("england-and-wales").get("events");
+            for (var event:events){
+                var date = event.get("date").asText();
+                System.out.println("date" + date);
+                }
+            System.out.println("events"+events);
 
         } catch (Exception e) {
 
